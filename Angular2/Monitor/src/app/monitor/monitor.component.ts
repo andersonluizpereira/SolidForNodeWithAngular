107c576649b10846ea0b9057380c8d3bb932ac55
acpereira <Anderson.Pereira-ext@fnac.com.br>
2017-06-20 16:08:24 -0300

    add files

--- Angular2/Monitor/src/app/monitor/monitor.component.ts
+++ Angular2/Monitor/src/app/monitor/monitor.component.ts
@@ -8,26 +8,52 @@ import { MonitorService } from "app/monitor/monitor.service";
   styleUrls: ['./monitor.component.css'],
 
   template: `
-  <div class="table table-bordered table-striped table-striped table-sm">
-     <table class="table" *ngFor="let disp of monitors">
-  <tr>
-    <th>
-       <h3>{{disp.DisplaySystem}}</h3>
-  
-   </th>
-  </tr>
- 
-  <td *ngFor="let item of disp.Items">
-     <tr>
-       <h3>{{item.DisplayName}}</h3>
-    </tr>
-   </td>
+   <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <div class="col col-lg-6" *ngFor="let disp of monitors">
+       
+        <div class="card">
+            
+            <table id="tableDisplay" class="table table-bordered table-striped table-striped table-sm " style="background-color: white;">
+                <thead class="card-header thead-inverse text-center">
+                    <tr>
+                        
+                        <th>
+                            Serviços
+                        </th>
+                        <th>
+                            Data Execução
+                        </th>
+                        <th>
+                            Hora Execução
+                        </th>
+                        
+                        <th class="text-center">
+                         Status
+                        </th>
 
-  
-  </table>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr  style="font-size: 20px; color: black;" *ngFor="let itens of disp.Items">
+                               <b> {{itens.DisplayName}}</b> <br/>
+                      <td>
+                                <b>{{itens.LastStartExecution | date:'dd/MM/yyyy'}}</b>
+                      
+                      </td>    
+                      <td>
+                                <b>{{itens.LastStartExecution | date:'HH:mm:ss'}}</b>
+                      </td>  
+                      <td>
+                          <span class=" fa fa-1x {{itens.IsAlive ? 'fa-arrow-circle-up greenyellowAlive' :'fa-arrow-circle-right yellowAlive'  }} ">
+                                </span>
+                      </td>  
+                    </tr>
 
-</div>
-  
+                </tbody>
+            </table>
+        </div>
+        
 `
    
 })
