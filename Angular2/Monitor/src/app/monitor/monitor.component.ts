3fd059b7e76b0b56e999b3abfbccb4ceef690f6e
acpereira <Anderson.Pereira-ext@fnac.com.br>
2017-06-28 17:41:36 -0300

    add files

--- Angular2/Monitor/src/app/monitor/monitor.component.ts
+++ Angular2/Monitor/src/app/monitor/monitor.component.ts
@@ -9,7 +9,7 @@ import { MonitorService } from "app/monitor/monitor.service";
 
   template: `
    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <div class="col col-lg-6" *ngFor="let disp of monitors">
        
         <div class="card">
