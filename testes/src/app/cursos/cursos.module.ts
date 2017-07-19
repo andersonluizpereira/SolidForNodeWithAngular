3d6f7c11d9213cf62a4a5833ea13dce6c157232e
acpereira <Anderson.Pereira-ext@fnac.com.br>
2017-07-19 15:24:57 -0300

    add files

---
+++ testes/src/app/cursos/cursos.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CursosComponent } from './cursos.component';
+import { CursosDetalheComponent } from './cursos-detalhe/cursos-detalhe.component';
+import { CursosService } from "app/cursos/cursos.service";
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [CursosComponent, CursosDetalheComponent],
+  exports:[CursosComponent],
+  providers : [CursosService]
+
+})
+export class CursosModule { }
