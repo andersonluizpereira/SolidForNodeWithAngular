b6257a46e091b20a496a13c22764c02ed77545ba
acpereira <Anderson.Pereira-ext@fnac.com.br>
2017-07-19 19:12:23 -0300

    add files

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -10,11 +10,41 @@ export class DataBindingComponent implements OnInit {
   url: string ='http://www.microsoft.com.br';
   urlImagem = 'http://lorempixel.com/400/200/sports';
   cursoAngular : boolean = true;
+  valorAtual: string ='';
+  valorSalvo: string ='';
+  isMouseOver: boolean = false;
+  nome: string='abc';
+
+  pessoa: any = {
+     nome:'def',
+     idade:20
+
+  }
+
+
+  onMouseOverOut(){
+  this.isMouseOver = !this.isMouseOver;
+    
+  }
+
+salvarValor(valor){
+  this.valorSalvo = valor;
+}
 
 getCurtiCurso(){
  return true;
 
 }
+
+ botaoClicado(){
+  alert('Botão clicado!')
+
+ }   
+
+ onKeyUp(evento : KeyboardEvent){
+  this.valorAtual = ((<HTMLInputElement>evento.target).value);
+  
+ }
 
   getValor(){
 
